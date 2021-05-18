860c7cb5473abfdd3a0111c28ee54e8750c0debd
sandydoo <hey@sandydoo.me>
2021-05-19 01:10:32 +0300

    Sync with remote changes

--- configuration.nix
+++ configuration.nix
@@ -2,11 +2,24 @@
 
 with lib;
 
+let
+  allowUnfree = { allowUnfree = true; };
+  unstable = import <unstable> { config = allowUnfree; };
+in
 {
   imports = [
     <nixpkgs/nixos/modules/virtualisation/virtualbox-image.nix>
     <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+    <unstable/nixos/modules/services/networking/tailscale.nix>
   ];
+
+  disabledModules = [ "services/networking/tailscale.nix" ];
+
+  nixpkgs.config = allowUnfree // {
+    packageOverrides = pkgs: {
+      tailscale = unstable.tailscale;
+    };
+  };
 
 # Mount a VirtualBox shared folder.
 # This is configurable in the VirtualBox menu at
@@ -16,6 +29,15 @@ with lib;
 #   device = "nameofdevicetomount";
 #   options = [ "rw" ];
 # };
+
+boot.kernelPackages = pkgs.linuxPackages_5_10;
+
+hardware = {
+  opengl.extraPackages = [ pkgs.intel-ocl ];
+  pulseaudio.enable = true;
+};
+
+virtualisation.virtualbox.guest.enable = true;
 
 users.users.sandydoo = {
   isNormalUser = true;
@@ -46,33 +68,60 @@ services.xserver = {
     };
   };
   desktopManager.gnome3.enable = true;
-  videoDrivers = mkOverride 40 [ "virualbox" "vmware" ];
+  videoDrivers = [ "vboxvideo" ];
 };
 
 # Set your time zone.
 time.timeZone = "Europe/Moscow";
 
+networking.firewall = {
+  enable = true;
+  trustedInterfaces = [ "tailscale0" ];
+  allowedUDPPorts = [ config.services.tailscale.port ];
+  allowedTCPPorts = [ 22 ];
+};
+
+networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
+
 services = {
   openssh = {
     enable = true;
+    allowSFTP = true;
     passwordAuthentication = false;
   };
+  sshd.enable = true;
   tailscale.enable = true;
   dbus.packages = [ pkgs.gnome3.dconf ];
   udev.packages = [ pkgs.gnome3.gnome-settings-daemon ];
 };
 
-hardware.pulseaudio.enable = true;
-
 # List packages installed in system profile. To search, run:
 # \$ nix search wget
 environment.systemPackages = with pkgs; [
   home-manager
+  fish
+  fd
   vim
+  kakoune
   git
-  tailscale
+  python3
+  openssl
+  dnsutils
+  nftables
+  nodejs-14_x
+  nodePackages.npm
+  nodePackages.yarn
+  unstable.tailscale
   glxinfo
+  google-chrome
   firefox
+  iperf3
+  openvpn
+  wireguard
+  wireguard-tools
+  ocl-icd
+  clinfo
+  linuxPackages.wireguard
   linuxPackages.virtualboxGuestAdditions
 ];
 
