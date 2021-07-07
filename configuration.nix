042674a8d62cd253f328d739a4abe886225f6768
sandydoo <hey@sandydoo.me>
2021-07-07 16:09:28 +0300

    Use new SSH key

--- configuration.nix
+++ configuration.nix
@@ -54,7 +54,7 @@ in
     shell = pkgs.fish;
     openssh = {
       authorizedKeys.keys = [
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDG4Q5+NEeBld93cAoJDjSLYna5+em6FdRjRrXCEqJ87yA9rNqZvs28SpJkZ2xfYY13FljSgI9u3WLRDYUwbRj8AeRLz51dmV5t3katVe7Ho6NH0rxpMxyPilFuwidZBBNGpDirmJQuej9k2zOgl+VOw8PkfqCPoystY6RGna+N1izywFcHPVtN2fdAHQLgdTZ5dQAwDi4whXeVtxRD0mFZUKI4OwjEUNPScueFmOdBbQG4z7nIbjYdq6QxjCcrWzfAKugpOKIcfQNFkKD6NKiYvobRYFo8IarD11HiPMz4eZ9kZIsI9104awOTAsxoXmBccww0s7rWsm3Nsr2i8Xgn8V2MxV3e/D1LxyFu2s0QJbJtPx4+POYOO78QzI3DihcmoMB9XwPx9m8U0349mgT3KtVSLqxI8a8ePhs4lDCtV4Jp34/mjg+piOPdrfiKLRlORxL+eq8SkOYkgtxvqn/+mB5HBRidxOKYpW/ya84nKyRPr+wufVJR6FBdUaVDGDs/nzALrQr7eWc3c9Yzib1HFanxErdzXp0eMRQu9FavFQuL3M4DlvovR5O9hEs64XUGQeNQTyRmb/wJTxP98dV04sEuUfPe2SzSJF0QwdqIVZNp0jLDww1GRZyrAG1FQ1bNpeeQ/lpd0tCYHY3H6w7+ZSMSCM632irUEqSb3QNvKQ== General key for Sander on asdf"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO18rhoNZWQZeudtRFBZvJXLkHEshSaEFFt2llG5OeHk hey@sandydoo.me"
       ];
     };
   };
