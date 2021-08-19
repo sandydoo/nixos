c167c49639358a96f5a2dedde9591167607e2415
sandydoo <hey@sandydoo.me>
2021-08-19 19:11:27 +0300

    Add home-manager flake

--- flake.nix
+++ flake.nix
@@ -2,15 +2,22 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
     nix-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/release-21.05";
   };
 
-  outputs = { self, nixpkgs, nix-unstable }: {
+  outputs = { self, nixpkgs, nix-unstable, home-manager }: {
     nix.registry.nixpkgs.flake = nixpkgs;
 
     nixosConfigurations.sandydoo = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.sandydoo = import ./home.nix;
+        }
         {
           # Let 'nixos-version --json' know about the Git revision
           # of this flake.
