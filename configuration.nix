6e5f1e6305833b484e9815f8a5e97a5c6529ff0a
sandydoo <hey@sandydoo.me>
2022-02-01 21:54:33 +0300

    Add default binary cache

--- configuration.nix
+++ configuration.nix
@@ -8,10 +8,12 @@ with lib;
   nix.package = pkgs.nixUnstable;
 
   nix.binaryCaches = [
+    "https://cache.nixos.org"
     "https://hydra.iohk.io"
     "https://iohk.cachix.org"
   ];
   nix.binaryCachePublicKeys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
     "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
   ];
