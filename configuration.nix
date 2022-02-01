5f0104e11a97ee50ca27d3509721cf5256c659d9
sandydoo <hey@sandydoo.me>
2022-02-01 16:21:14 +0300

    Add IOHK binary caches

--- configuration.nix
+++ configuration.nix
@@ -7,6 +7,15 @@ with lib;
 
   nix.package = pkgs.nixUnstable;
 
+  nix.binaryCaches = [
+    "https://hydra.iohk.io"
+    "https://iohk.cachix.org"
+  ];
+  nix.binaryCachePublicKeys = [
+    "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
+  ];
+
   nix.extraOptions = ''
     keep-derivations = true
     keep-outputs = true
