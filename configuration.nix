a53cb2f65061e7e4ec66787a7eb5877260c904c3
sandydoo <hey@sandydoo.me>
2022-02-25 08:47:33 +0300

    Enable store optimisation
    Let’s try this setting again. It saves space, but the last time I
    enabled it ended with me having to completely reinstall Nix...

--- configuration.nix
+++ configuration.nix
@@ -18,6 +18,8 @@ with lib;
     "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
   ];
 
+  nix.autoOptimiseStore = true;
+
   nix.extraOptions = ''
     keep-derivations = true
     keep-outputs = true
