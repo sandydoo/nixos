21e9b8ed09da3d1c224ddc398f2bab8c05e45e36
sandydoo <hey@sandydoo.me>
2022-06-08 13:48:42 +0000

    Upgrade to Nix 22.05

--- flake.nix
+++ flake.nix
@@ -1,42 +1,45 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     nix-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-21.11";
+    home-manager.url = "github:nix-community/home-manager/release-22.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nix-unstable, home-manager }@inputs: {
-    formatter.x86_64-linux = nix-unstable.legacyPackages.x86_64-linux.nixfmt;
+  outputs = { self, nixpkgs, nix-unstable, home-manager }@inputs:
+    let system = "x86_64-linux";
+    in {
+      formatter.${system} = nix-unstable.legacyPackages.${system}.nixfmt;
 
-    nixosConfigurations.superstrizh = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      nixosConfigurations.superstrizh = nixpkgs.lib.nixosSystem {
+        inherit system;
 
-      modules = [
-        ./machines/superstrizh/configuration.nix
-        ./users/sandydoo.nix
-        # ./gnome.nix
-        ./i3.nix
-        ./modules/tailscale.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.sandydoo = import ./users/sandydoo/home.nix;
-        }
-        {
-          # Let 'nixos-version --json' know about the Git revision
-          # of this flake.
-          system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
-        }
-      ];
+        modules = [
+          ./machines/superstrizh/configuration.nix
+          ./users/sandydoo.nix
+          # ./gnome.nix
+          ./i3.nix
+          ./modules/tailscale.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.sandydoo = import ./users/sandydoo/home.nix;
+          }
+          {
+            # Let 'nixos-version --json' know about the Git revision
+            # of this flake.
+            system.configurationRevision =
+              nixpkgs.lib.mkIf (self ? rev) self.rev;
+          }
+        ];
 
-      specialArgs = inputs // {
-        unstable = import nix-unstable {
-          system = "x86_64-linux";
-          config.allowUnfree = true;
+        specialArgs = inputs // {
+          unstable = import nix-unstable {
+            inherit system;
+            config.allowUnfree = true;
+          };
         };
       };
     };
-  };
 }
