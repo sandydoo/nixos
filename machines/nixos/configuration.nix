123e71ed901f5460716baa0e7649db96de33d9d2
sandydoo <hey@sandydoo.me>
2023-01-09 09:58:16 +0000

    Add unofficial arch cache

--- machines/nixos/configuration.nix
+++ machines/nixos/configuration.nix
@@ -70,6 +70,7 @@
     "cachix-private.cachix.org-1:3axMmTI11ok4U2nMmWX8MZsRLmQzQBuRdOJ0EszhPuY="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
   ];
 
   nix.settings.substituters = [
@@ -77,6 +78,7 @@
     "https://cachix-private.cachix.org"
     "https://nix-community.cachix.org"
     "https://cache.iog.io"
+    "https://cache.zw3rk.com"
   ];
 
   nix.settings.system-features = ["kvm" "big-parallel"];
