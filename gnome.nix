a60c157e0354dd4acd133cb4b32fa81695b94248
sandydoo <hey@sandydoo.me>
2023-03-07 14:13:31 +0000

    Disable gnome settings

--- gnome.nix
+++ gnome.nix
@@ -4,6 +4,7 @@
   services.xserver = {
     enable = true;
     desktopManager.gnome.enable = true;
+    displayManager.defaultSession = "gnome";
     displayManager.gdm.enable = true;
     displayManager.gdm.wayland = true;
     displayManager.gdm.autoSuspend = false;
@@ -13,8 +14,8 @@
     };
   };
 
-  services.dbus.packages = [ pkgs.dconf ];
-  services.udev.packages = [ pkgs.gnome.gnome-settings-daemon ];
+  # services.dbus.packages = [ pkgs.dconf ];
+  # services.udev.packages = [ pkgs.gnome.gnome-settings-daemon ];
 
   programs.xwayland.enable = false;
 }
