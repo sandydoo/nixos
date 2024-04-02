bdf0b532fd3d4d8c4f2e99915cecca76cb4220a8
Sander <hey@sandydoo.me>
2024-04-02 06:15:22 +0000

    sway: update config

--- modules/sway.nix
+++ modules/sway.nix
@@ -1,9 +1,21 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  programs.waybar.enable = true;
+  services.xserver = {
+    enable = true;
+
+    displayManager = {
+      defaultSession = "sway";
+      lightdm.enable = true;
+      autoLogin = {
+        enable = true;
+        user = "sandydoo";
+      };
+    };
+  };
 
   programs.sway.enable = true;
-
   programs.xwayland.enable = true;
+
+  environment.systemPackages = [ pkgs.wdisplays ];
 }
