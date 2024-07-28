a548b4d57d04bb080494e21725c15d0ff657a7f4
Sander <hey@sandydoo.me>
2024-07-28 11:52:52 +0000

    sway: remove xserver and use greetd as login manager

--- modules/sway.nix
+++ modules/sway.nix
@@ -1,21 +1,21 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
-  services.displayManager = {
-    defaultSession = "sway";
-    autoLogin = {
-      enable = true;
-      user = "sandydoo";
-    };
-  };
-
-  services.xserver = {
-    enable = true;
-    displayManager.lightdm.enable = true;
-  };
-
   programs.sway.enable = true;
   programs.xwayland.enable = true;
+
+  security.polkit.enable = true;
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session.command = "${lib.getExe pkgs.greetd.tuigreet} --time --cmd sway";
+      initial_session = {
+        command = "sway";
+        user = "sandydoo";
+      };
+    };
+  };
 
   environment.systemPackages = [ pkgs.wdisplays ];
 }
