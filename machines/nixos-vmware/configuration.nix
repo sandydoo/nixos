df9e3d396cf105fd052e530cb0b4ab1e48e971ed
Sander <hey@sandydoo.me>
2024-10-12 15:51:15 +0000

    vmware: add datadog-agent config to test otlp support

--- machines/nixos-vmware/configuration.nix
+++ machines/nixos-vmware/configuration.nix
@@ -48,4 +48,23 @@
       xrandr --output Virtual-1 --auto
     '')
   ];
+
+  services.datadog-agent = {
+    enable = false;
+    enableTraceAgent = true;
+    site = "datadoghq.eu";
+    apiKeyFile = "/run/datadog-agent";
+    package = pkgs.datadog-agent.override { extraTags = [ "otlp" ]; };
+    extraConfig = {
+      logs_enabled = true;
+      otlp_config = {
+        receiver = {
+          protocols = {
+            http.endpoint = "localhost:4318";
+            grpc.endpoint = "localhost:4317";
+          };
+        };
+      };
+    };
+  };
 }
