134e959bb2f68f3aa7f543929e7b48ca26dd8f5f
Sander <hey@sandydoo.me>
2024-12-29 14:12:18 +0400

    home: migrate gpg-agent and profile

--- machines/asdfpro/configuration.nix
+++ machines/asdfpro/configuration.nix
@@ -11,20 +11,6 @@ let
     '';
   };
 
-  pinentry-custom = pkgs.writeShellScriptBin "pinentry-custom" ''
-    pinentry=${lib.getExe pkgs.pinentry-tty}
-    case "$PINENTRY_USER_DATA" in
-    *USE_TTY*)  pinentry=${lib.getExe pkgs.pinentry-tty} ;;
-    *USE_CURSES*) pinentry=${lib.getExe pkgs.pinentry-curses} ;;
-    ${lib.optionalString pkgs.stdenv.isLinux ''
-    *USE_GNOME3*) pinentry=${lib.getExe pkgs.pinentry-gnome} ;;
-    ''}
-    ${lib.optionalString pkgs.stdenv.isDarwin ''
-    *USE_MAC*) pinentry=${lib.getBin pkgs.pinentry_mac}/Applications/pinentry-mac.app/Contents/MacOS/pinentry-mac ;;
-    ''}
-    esac
-    exec $pinentry "$@"
-  '';
 in
 {
   home-manager.useUserPackages = true;
@@ -92,7 +78,6 @@ in
     pandoc
 
     gnupg
-    pinentry-custom
     blackbox
 
     cmus
--- users/sandydoo/systems/darwin.nix
+++ users/sandydoo/systems/darwin.nix
@@ -1,7 +1,52 @@
-{ ... }: 
+{ pkgs, lib, ... }:
 
+let
+  pinentry-custom = pkgs.writeShellScriptBin "pinentry-custom" ''
+    pinentry=${lib.getExe pkgs.pinentry-tty}
+    case "$PINENTRY_USER_DATA" in
+    *USE_TTY*)  pinentry=${lib.getExe pkgs.pinentry-tty} ;;
+    *USE_CURSES*) pinentry=${lib.getExe pkgs.pinentry-curses} ;;
+    ${lib.optionalString pkgs.stdenv.isLinux ''
+    *USE_GNOME3*) pinentry=${lib.getExe pkgs.pinentry-gnome} ;;
+    ''}
+    ${lib.optionalString pkgs.stdenv.isDarwin ''
+    *USE_MAC*) pinentry=${lib.getBin pkgs.pinentry_mac}/Applications/pinentry-mac.app/Contents/MacOS/pinentry-mac ;;
+    ''}
+    esac
+    exec $pinentry "$@"
+  '';
+in
 {
-  home.file.".config/hammerspoon".source = ./hammerspoon;
+  home.packages = [ pinentry-custom ];
+
+  home.file.".hammerspoon/init.lua".source = ../hammerspoon/init.lua;
+
+  home.sessionVariablesExtra = ''
+    export VOLTA_HOME="$HOME/.volta"
+    export PATH="$VOLTA_HOME/bin:$PATH"
+
+    export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+  '';
+
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
+
+    # Effectively disable cache expiry
+    defaultCacheTtl = 34560000;
+    defaultCacheTtlSsh = 34560000;
+    maxCacheTtl = 34560000;
+    maxCacheTtlSsh = 34560000;
+
+    sshKeys = [
+      "FB9E85EC1136B7841BCAADDEDF430EB22F3F90E3"
+    ];
+
+    extraConfig = ''
+      allow-loopback-pinentry
+      pinentry-program ${toString pinentry-custom}/bin/pinentry-custom
+    '';
+  };
 
   home.stateVersion = "24.11";
 }
