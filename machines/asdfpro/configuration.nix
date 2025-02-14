8236743d7e8d6b4081dbf01f68b92b01972bea46
Sander <hey@sandydoo.me>
2025-02-14 17:22:39 +0400

    asdf: simplify nix settings

--- machines/asdfpro/configuration.nix
+++ machines/asdfpro/configuration.nix
@@ -191,13 +191,11 @@ in
     keep-outputs = false
     experimental-features = nix-command flakes
   '';
-  nix.settings.trusted-users = [ "root" "sander" ];
+  nix.settings.trusted-users = [ "sander" ];
   nix.settings.substituters = [
-    "https://cache.nixos.org"
     "https://nix-community.cachix.org"
   ];
   nix.settings.trusted-public-keys = [
-    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
   ];
 
