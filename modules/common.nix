e99670586303138e2c30e87f56b51099d5ba1b56
Sander <hey@sandydoo.me>
2025-02-14 13:24:17 +0000

    common: bump nix to 2.25

--- modules/common.nix
+++ modules/common.nix
@@ -30,7 +30,7 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
-  nix.package = pkgs.nixVersions.nix_2_23;
+  nix.package = pkgs.nixVersions.nix_2_25;
 
   # Disable channels entirely.
   # Requires removing all the channel files and symlinks manually.
