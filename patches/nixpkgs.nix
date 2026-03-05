ea5230e371e3ea1dd65f35c342a54a00baaa514d
Sander <hey@sandydoo.me>
2026-03-05 16:33:35 +0100

    macos: drop yt-dlp patch

--- patches/nixpkgs.nix
+++ patches/nixpkgs.nix
@@ -2,14 +2,7 @@
 {
   common = [ ];
 
-  darwin = [
-    # yt-dlp: exclude secretstorage dependency on Darwin
-    # https://github.com/NixOS/nixpkgs/pull/493943
-    (fetchpatch {
-      url = "https://github.com/NixOS/nixpkgs/pull/493943.patch";
-      hash = "sha256-skGGebqoQ9ouT7qJsIg5EQz3v+9VRxoYJ1o/8zRPHto=";
-    })
-  ];
+  darwin = [ ];
 
   linux = [ ];
 }
