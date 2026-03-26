da30a48ea060c37de0a2fdc94ae57c7f5dc61c05
Sander <hey@sandydoo.me>
2026-03-27 00:20:37 +0100

    patches: fix patch
    Don't use the merge commit.

--- patches/nixpkgs.nix
+++ patches/nixpkgs.nix
@@ -3,7 +3,7 @@
   common = [
     (fetchpatch {
       # direnv: no external linkmode
-      url = "https://github.com/NixOS/nixpkgs/commit/d6f179cad8ac8f752264a7ed4fa7e3c9a1f5c2c1.patch";
+      url = "https://github.com/NixOS/nixpkgs/commit/a4fb16db2751d9c9e5f3512c697d2ac49d406789.patch";
       hash = "sha256-5gSEUjFiQcV7XpsiqfYg2v78JiK7i9ttM3x8JJ/K1pY=";
     })
   ];
