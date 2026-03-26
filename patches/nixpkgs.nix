3b9fc5b1053a06460bafbe02f06b42522eb249d9
Sander <hey@sandydoo.me>
2026-03-27 00:34:42 +0100

    patches: drop direnv patch

--- patches/nixpkgs.nix
+++ patches/nixpkgs.nix
@@ -1,12 +1,6 @@
 { fetchpatch }:
 {
-  common = [
-    (fetchpatch {
-      # direnv: no external linkmode
-      url = "https://github.com/NixOS/nixpkgs/commit/a4fb16db2751d9c9e5f3512c697d2ac49d406789.patch";
-      hash = "sha256-lGexSgro3iG3KwHOrjNricJPd5brJyoO6B6xg794VXo=";
-    })
-  ];
+  common = [ ];
 
   darwin = [ ];
 
