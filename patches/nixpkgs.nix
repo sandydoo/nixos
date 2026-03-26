b39c765cdec8a9d4b85d2ade0300b3d65f785c20
Sander <hey@sandydoo.me>
2026-03-27 00:29:39 +0100

    patches: fix the hash again

--- patches/nixpkgs.nix
+++ patches/nixpkgs.nix
@@ -4,7 +4,7 @@
     (fetchpatch {
       # direnv: no external linkmode
       url = "https://github.com/NixOS/nixpkgs/commit/a4fb16db2751d9c9e5f3512c697d2ac49d406789.patch";
-      hash = "sha256-5gSEUjFiQcV7XpsiqfYg2v78JiK7i9ttM3x8JJ/K1pY=";
+      hash = "sha256-lGexSgro3iG3KwHOrjNricJPd5brJyoO6B6xg794VXo=";
     })
   ];
 
