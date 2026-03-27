0a20e8c6c4c6e71c306df199bb1274bd5bdd83fb
Sander <hey@sandydoo.me>
2026-03-27 19:33:13 +0100

    patches: fix direnv on darwin AGAIN

--- patches/nixpkgs.nix
+++ patches/nixpkgs.nix
@@ -2,7 +2,13 @@
 {
   common = [ ];
 
-  darwin = [ ];
+  darwin = [
+    (fetchpatch {
+      # direnv: no external linkmode
+      url = "https://github.com/NixOS/nixpkgs/commit/a4fb16db2751d9c9e5f3512c697d2ac49d406789.patch";
+      hash = "sha256-lGexSgro3iG3KwHOrjNricJPd5brJyoO6B6xg794VXo=";
+    })
+  ];
 
   linux = [ ];
 }
