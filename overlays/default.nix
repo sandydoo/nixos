a63abf544ec26f032508c50b59d1c9e3488d6bea
Sander <hey@sandydoo.me>
2026-04-24 13:25:13 +0200

    streamlink: fix builds on macOS

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,1 +1,13 @@
-final: prev: { }
+final: prev: {
+  streamlink = prev.streamlink.overridePythonAttrs (old: {
+    disabledTests =
+      (old.disabledTests or [ ])
+      ++ final.lib.optionals final.stdenv.hostPlatform.isDarwin [
+        # requires Linux-only socket.SO_BINDTODEVICE
+        "test_set_interface[unix-iface]"
+        "test_set_interface[unix-iface-prefix]"
+        "test_set_interface[unix-ifhost-prefix]"
+        "test_set_interface[unix-ifhost-prefix-invalid]"
+      ];
+  });
+}
