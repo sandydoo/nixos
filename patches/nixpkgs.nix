c883c8691445c08d0cb0ca5a221abb1ff65d75c3
Sander <hey@sandydoo.me>
2026-05-02 18:15:43 +0200

    patches: drop

--- patches/nixpkgs.nix
+++ patches/nixpkgs.nix
@@ -3,10 +3,6 @@
   common = [ ];
 
   darwin = [
-    (fetchpatch {
-      url = "https://github.com/NixOS/nixpkgs/commit/a25cd2de2d32b1ac1bb752b3ccccfe55a7a5019d.patch";
-      hash = "sha256-KMKnApUzF90EC4VqKelOIEvRbfLDxW7ZpSd/oFflb4M=";
-    })
   ];
 
   linux = [ ];
